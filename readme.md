6d643f3941d7fccff173c9755fc2d0326f86c3ad
Troy Washington <troyvw96@gmail.com>
2024-10-07 20:03:34 -0700

    Submission message

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 Add the following to this `README.md` file:  
   * A short report explaining your design decisions and any challenges faced.
   * Documentation: Clear and concise documentation on how to run your application.
+
+  First issue was that the variables loggedIn, user, and userRole were undefined. Those variables shoudl be defined outside of the checkAccess function for future reference and given a place holder. Second issue is the same issue as the first but with the message variable. The message variable should be defined outside of the checkAcess function for consistency and clarity. Third and last issue is the foro loop at the end. The loop is placed outside of the checkAccess function and is never ran. The loop should be placed in the last branch of the function just above or below the "return accessLevel" line of code. 
--- refactoring.js
+++ refactoring.js
@@ -1,3 +1,15 @@
+
+
+
+
+
+// variables loggedIn, user, and userRole were undefined.
+// 
+let loggedIn = true;
+let user = "John Doe";
+const userRole = "admin";
+
+
 function checkAccess() {
     var accessLevel;
     if (loggedIn) {
@@ -15,9 +27,12 @@ function checkAccess() {
     }
     return accessLevel;
 }
-
+// This b
 for (var i = 0; i < 3; i++) {
     console.log("Attempt", i);
 }
 
 console.log("Access Level:", checkAccess());
+
+// First issue was that the variables loggedIn, user, and userRole were undefined. Those variables shoudl be defined outside of the checkAccess function for future reference and given a place holder. Second issue is the same issue as the first but with the message variable. The message variable should be defined outside of the checkAcess function for consistency and clarity. Third and last issue is the foro loop at the end. The loop is placed outside of the checkAccess function and is never ran. The loop should be placed in the last branch of the function just above or below the "return accessLevel" line of code. 
+
